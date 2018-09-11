be83557b74c29dacb431a8dac2b84660cdf4978a
Daniel Costa <danieljunnio@hotmail.com>
2018-09-10 21:43:08 -0300

    Add files via upload

---
+++ src/providers/buscar-todos/buscar-todos.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AutenticarProvider} from '../autenticar/autenticar';
+import { MoradiaDto } from '../../Model/moradiaDto';
+
+@Injectable()
+export class BuscarTodosProvider {
+
+  constructor(public afDB : AngularFireDatabase,
+              public auth: AutenticarProvider) {
+
+}
+
+private getPath()
+{
+return ''; 
+}
+
+getNomeMoradia(nomeMoradia : string)
+{
+return this.afDB.list<MoradiaDto>(this.getPath(),
+ref => 
+ref.orderByChild('NomeMoradia')
+.equalTo(nomeMoradia)
+).valueChanges();
+}
+
+getMoradiaInicio(nomeMoradia : string)
+{
+return this.afDB.list<MoradiaDto>(this.getPath(),
+ref => 
+ref.orderByChild('NomeMoradia')
+.startAt(nomeMoradia)
+.endAt(nomeMoradia + "z")
+).valueChanges();
+}
+
+get(key: string) {
+return this.afDB.list<MoradiaDto>(
+this.getPath() + "/" + key).valueChanges();
+}  
+
+getMoradias() {
+return this.afDB.list<MoradiaDto>(this.getPath(), ref => ref.orderByChild('NomeMoradia'))
+.valueChanges();     
+}
+
+}
---
+++ src/providers/cep/cep.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpModule, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { MoradiaDto } from '../../Model/moradiaDto';
+
+@Injectable()
+export class CepProvider {
+
+  constructor(private http: Http) {
+    
+  }
+
+  buscar(cep:string){
+    return this.http.get(`https://viacep.com.br/ws/${cep}/json/`)
+              .toPromise()
+              .then(response => {
+                return this.converterResposta(response.json())
+              });
+  }
+
+  private converterResposta(cepResposta): MoradiaDto{
+    let moradiaDto = new MoradiaDto();
+    moradiaDto.CEP = cepResposta.cep;
+    moradiaDto.Logradouro = cepResposta.logradouro;
+    moradiaDto.Bairro = cepResposta.bairro;
+    moradiaDto.Cidade = cepResposta.localidade;
+    moradiaDto.Estado = cepResposta.uf;
+
+    moradiaDto.IdMoradia = 0;
+    moradiaDto.NomeMoradia = "";
+    moradiaDto.DescricaoMoradia = "";
+    moradiaDto.Contato = 0;
+    moradiaDto.Pais = "Brasil";
+    moradiaDto.ValorAluguel = 0;
+    moradiaDto.Imagem = "";
+    moradiaDto.Numero = 0;
+
+    return moradiaDto;
+  }
+
+}
